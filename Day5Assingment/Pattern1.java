13a247eca97e421c955c064f2c6c7946257be354
musalesanket17 <musalesanket17@gmail.com>
2023-03-28 23:27:01 +0530

    Day5_Pattern1.java

---
+++ Day5Assingment/Pattern1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner ;
+class Pattern1{
+
+
+  public static void main(String args[]){
+	 Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the Number : ");
+	 int num = sc.nextInt();
+    for(int i=1;i<=num;i++){
+		
+		for(int j=1;j<=i;j++){
+			
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+  }
+ 
+}
