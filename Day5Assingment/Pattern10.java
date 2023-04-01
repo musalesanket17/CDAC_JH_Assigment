6f319b5a22b5ba450a70d993e751bc0c53cde83a
musalesanket17 <musalesanket17@gmail.com>
2023-04-01 14:44:03 +0530

    Day5As10

---
+++ Day5Assingment/Pattern10.java
@@ -0,0 +1,20 @@
+class Pattern10{
+	
+	public static void main(String args[]){
+		
+		for(int i=1;i<=5;i++){
+			
+			for(int j=1;j<=i;j++){
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
+/*
+1
+22
+333
+4444
+55555
+*/
