685143bb8260912f70c499653eed9bf28ca9ecd8
musalesanket17 <musalesanket17@gmail.com>
2023-04-03 17:04:35 +0530

    Day5_Ass15

---
+++ Day5Assingment/Pattern15.java
@@ -0,0 +1,20 @@
+class Pattern15{
+
+ public static void main(String args[]){
+  int d=64;
+  
+     for(int i=1;i<=5;i++){
+		 
+		 for(int j=1;j<=5-i;j++){
+			 
+			 System.out.print(" ");
+		 }
+		 for(int k=1;k<=i;k++){
+			 System.out.print((char)(d+k)+" ");
+		 }
+		 System.out.println();
+	 }
+ }
+
+
+}
