bed4e87837d17f2e94c86a1a35bdb4afbc4e3a99
musalesanket17 <musalesanket17@gmail.com>
2023-04-04 14:52:25 +0530

    Day5As22

---
+++ Day5Assingment/Pattern22.java
@@ -0,0 +1,17 @@
+class Pattern22{
+
+
+   public static void main(String args[]){
+   
+       int counter =0;
+	   for(int i=1;i<=5;i++){
+		   
+		   for(int j=1;j<=i;j++){
+			   counter++;
+			   System.out.print(counter+" ");
+		   }
+		   System.out.println();
+	   }
+	   
+   }
+}
