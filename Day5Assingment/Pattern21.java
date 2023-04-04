75624ff208be0e1105e49337678e458b157db5c0
musalesanket17 <musalesanket17@gmail.com>
2023-04-04 14:51:40 +0530

    Day5As21

---
+++ Day5Assingment/Pattern21.java
@@ -0,0 +1,13 @@
+class Pattern21{
+
+     public static void main(String args[]){
+	 
+	    for(int i=5;i>=1;i--){
+			
+			for(int j=5;j>=i;j--){
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	 }
+}
