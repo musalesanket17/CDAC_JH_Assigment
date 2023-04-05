86d1ee1b26a644a9b83a5942f4a876239ddb9c31
musalesanket17 <musalesanket17@gmail.com>
2023-04-05 13:29:35 +0530

    Day5_Aass2s

---
+++ Day5Assingment/Pattern26.java
@@ -0,0 +1,31 @@
+class Pattern26{
+ 
+    public static void main(String args[]){
+	 
+	 for(int i=1;i<=5;i++){
+		 
+		 for(int j=5;j>=i;j--){
+			 System.out.print(" ");
+		 }
+		 for(int k=1;k<=2*i-1;k++){
+			 
+			 System.out.print("*");
+		 }
+		 System.out.println();
+
+	 }
+	    
+		for(int i=4;i>=1;i--){
+			
+			for( int j=5;j>=i;j--){
+				
+				System.out.print(" ");
+				
+			}
+			for(int k=1;k<=2*i-1;k++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
