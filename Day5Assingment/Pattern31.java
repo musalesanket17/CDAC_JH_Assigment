35ed0190f0fa3b835ec6d5ffa1bb3263e95939fa
musalesanket17 <musalesanket17@gmail.com>
2023-04-06 18:33:48 +0530

    Add 30-35

---
+++ Day5Assingment/Pattern31.java
@@ -0,0 +1,30 @@
+class Pattern31{
+
+   public static void main(String args[]){
+   
+      for(int i=5;i>=1;i--){
+		  
+		  for(int j=5;j>=i;j--){
+			  System.out.print(" ");
+		  }
+		  
+		  for(int k=1;k<=i;k++){
+			  
+			  System.out.print("* ");
+		  }
+		  System.out.println();
+	  }
+	  for(int i=2;i<=5;i++){
+		  
+		  for(int j=5;j>=i;j--){
+			  System.out.print(" ");
+		  }
+		  
+		  for(int k=1;k<=i;k++){
+			  
+			  System.out.print("* ");
+		  }
+		  System.out.println();
+	  }
+   }
+}
